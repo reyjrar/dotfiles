d066837ec054fd026e4b28f88cc8d2c8d5f8f67c
Brad Lhotsky <brad.lhotsky@gmail.com>
2011-06-22 13:46:13 -0400

    installer script works

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+function rel2abs() {
+	local relative=$1
+	local cwd=$(pwd);
+
+	cd $relative;
+	absolute=$(pwd);
+	cd $cwd;
+
+	echo $absolute;
+}
+
 bindir=`dirname $0`;
-echo "we are $0 in $bindir";
+basedir=`rel2abs ${bindir/bin}`;
+
+for rc in `ls -1 $basedir`; do
+	if [ -f $rc ] && [ "$rc" != "README" ]; then
+		final="$HOME/.$rc";
+		whereilive="$basedir/$rc";
+		echo -n "setting up $final .. ";
+		if [ -f $final ] || [ -L $final ]; then
+			echo -n "removing current file .. ";
+			rm -f $final;
+		fi;
+		echo -n "linking to $whereilive .. ";
+		ln -s $whereilive $final;
+		echo "done.";
+	fi;
+done;
+
+
