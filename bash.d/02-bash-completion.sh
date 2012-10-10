d18ad2807e8fabd33742995cdadcdd8cfa046431
Brad Lhotsky <brad.lhotsky@booking.com>
2012-10-10 11:38:31 +0200

    Updated to work with MacPorts 2.1.2

--- bash.d/02-bash-completion.sh
+++ bash.d/02-bash-completion.sh
@@ -1,10 +1,13 @@
 # Source global definitions
 if [ -f /etc/bash_completion ]; then
+    # Linux
     . /etc/bash_completion
+elif [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
+    # MacPorts 2.1.2
+    . /opt/local/etc/profile.d/bash_completion.sh
 elif [ -f /opt/local/etc/bash_completion ]; then
+    # MacPorts prior to 2.1.2
     . /opt/local/etc/bash_completion
-else
-    echo "[notify] - bash completion disabled"
 fi
 
 shopt -s no_empty_cmd_completion
