ba7666962b6168436aaf488128c0b9df36c15746
Brad Lhotsky <brad.lhotsky@booking.com>
2013-02-04 15:03:52 +0100

    Disabled caching/proxy wrapper stuff Fixed BASH completion to source the git definitions properly.

--- bash.d/02-bash-completion.sh
+++ bash.d/02-bash-completion.sh
@@ -1,13 +1,17 @@
 # Source global definitions
-if [ -f /etc/bash_completion ]; then
-    # Linux
-    . /etc/bash_completion
-elif [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
-    # MacPorts 2.1.2
-    . /opt/local/etc/profile.d/bash_completion.sh
-elif [ -f /opt/local/etc/bash_completion ]; then
-    # MacPorts prior to 2.1.2
-    . /opt/local/etc/bash_completion
-fi
+
+COMPLETION_SCRIPTS="
+    /etc/bash_completion
+    /opt/local/etc/profile.d/bash_completion.sh
+    /opt/local/etc/bash_completion
+    /etc/bash_completion.d/git
+"
+
+for file in $COMPLETION_SCRIPTS; do
+    if [ -f $file ]; then
+        (($DEBUG)) && echo "bash_completion: sourcing $file";
+        . $file
+    fi
+done
 
 shopt -s no_empty_cmd_completion
