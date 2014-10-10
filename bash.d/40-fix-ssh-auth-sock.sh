227166a7f53c1ae2ddee43edb184852629ffd4c1
Brad Lhotsky <brad.lhotsky@booking.com>
2014-10-10 17:48:00 +0200

    If ~/.ssh doesn't exist, create it.

--- bash.d/40-fix-ssh-auth-sock.sh
+++ bash.d/40-fix-ssh-auth-sock.sh
@@ -1,4 +1,7 @@
 # Fix SSH auth socket location so agent forwarding works with tmux
+if ! test -d ~/.ssh; then
+    mkdir -m 0700 ~/.ssh
+fi
 if test "$SSH_AUTH_SOCK" ; then
     ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
 fi
