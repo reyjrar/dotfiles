c0def4bf7614b4d44678d625c0e243f8589a2673
Brad Lhotsky <brad@divisionbyzero.net>
2020-04-09 08:28:28 -0700

    FZF and GitLFS
    * Move FZF later so it's not overwritten by the defaults
    * Setup GitLFS for things

--- bash.d/01-shell-features.sh
+++ bash.d/01-shell-features.sh
@@ -1,6 +1,2 @@
 # Enable / Disable Shell features
 shopt -s autocd cdspell dirspell dotglob nocaseglob
-
-if [ -e /opt/local/share/fzf/shell/key-bindings.bash ]; then
-    . /opt/local/share/fzf/shell/key-bindings.bash
-fi
---
+++ bash.d/50-fzf.sh
@@ -0,0 +1,4 @@
+# Load FZF if available
+if [ -e /opt/local/share/fzf/shell/key-bindings.bash ]; then
+    . /opt/local/share/fzf/shell/key-bindings.bash
+fi
