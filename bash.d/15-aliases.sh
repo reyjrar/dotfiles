be3c5a5529321a9daac077ebbbe58c7c420d9d21
Brad Lhotsky <brad@divisionbyzero.net>
2022-04-16 11:56:06 -0700

    Update aliases to use new tools
    * If `exa` is installed, use it in place of `ls`
    * If `bat` is installed, use it in place of `cat`

--- bash.d/15-aliases.sh
+++ bash.d/15-aliases.sh
@@ -3,12 +3,6 @@ alias who='who -H -u -T'
 alias c="contents"
 # Get Aliases
 alias gg='git grep'
-# Hive
-command -v rlwrap &> /dev/null
-rc=$?
-if [[ "$rc" -eq "0" ]]; then
-        alias rlhive='rlwrap -i --prompt-colour=yellow -f ~/.hive_completion -a hive $*'
-fi;
 
 if [ "$HOSTOS" == "OpenBSD" ] || [ "$HOSTOS" == "FreeBSD" ]; then
     if [ -x "/usr/local/bin/gls" ]; then
@@ -17,7 +11,15 @@ if [ "$HOSTOS" == "OpenBSD" ] || [ "$HOSTOS" == "FreeBSD" ]; then
         alias ls="ls -FG -D '%F %T'"
     fi
 else
-    alias ls='ls --time-style=long-iso -F --color'
+    if which exa &> /dev/null; then
+        alias ls='exa --time-style=long-iso -F --color=auto --icons'
+    else
+        alias ls='ls --time-style=long-iso -F --color'
+    fi
+fi
+
+if which bat &> /dev/null; then
+    alias cat='bat --paging=never'
 fi
 
 if which neomutt &> /dev/null; then
