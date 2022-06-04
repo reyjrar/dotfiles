167df28c5375c5e74e46b37d492a63c7e6bc14b1
Brad Lhotsky <brad@divisionbyzero.net>
2022-06-04 10:33:24 -0700

    Add brew bash-completion

--- bash.d/03-homebrew.sh
+++ bash.d/03-homebrew.sh
@@ -1,11 +1,13 @@
 if [ -x "/opt/homebrew/bin/brew" ]; then
+    # Set Paths
+    path_inject /opt/homebrew/bin
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
-    # Homebrew bin
-    path_inject /opt/homebrew/bin
     # Inject gnubins therein
     for path in /opt/homebrew/opt/*/libexec/gnubin; do
         path_inject "$path"
     done
+
+    [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
 fi
 
