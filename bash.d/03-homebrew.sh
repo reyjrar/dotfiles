192796f264f727cf48436ef7a760a4ea36511bb0
Brad Lhotsky <brad@divisionbyzero.net>
2023-09-23 11:58:57 -0700

    Make homebrew setup smarter

--- bash.d/03-homebrew.sh
+++ bash.d/03-homebrew.sh
@@ -1,12 +1,16 @@
-if [ -x "/opt/homebrew/bin/brew" ]; then
+brew_install_dir="/opt/homebrew"
+if [ -x "$brew_install_dir/bin/brew" ]; then
     # Set Paths
-    path_inject /opt/homebrew/bin
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+    path_inject "$brew_install_dir/bin"
+    eval "$($brew_install_dir/bin/brew shellenv)"
 
     # Inject gnubins therein
-    for path in /opt/homebrew/opt/*/libexec/gnubin; do
+    for path in $brew_install_dir/opt/*/libexec/gnubin; do
         path_inject "$path"
     done
 
-    [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+    for p in "bash_completion.sh" "z.sh"; do
+        profile="$brew_install_dir/etc/profile.d/$p"
+        [[ -r "$profile" ]] && . "$profile"
+    done
 fi
