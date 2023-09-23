da434795212986d8a50f2f1299e79a3bcf0cfe6d
Brad Lhotsky <brad@divisionbyzero.net>
2023-09-23 11:44:27 -0700

    Add go bash completions

--- bash.d/95-golang.sh
+++ bash.d/95-golang.sh
@@ -3,4 +3,9 @@ if [ -d "$HOME/go" ]; then
     export GOPATH="$HOME/go"
     export GOBIN="$GOPATH/bin"
     path_inject "$GOBIN"
+
+    # Go completions
+    if command -v gocomplete &> /dev/null; then
+        complete -C gocomplete go
+    fi
 fi
