12844aa4f80f6b905e51b29e5c97131c3812df08
Brad Lhotsky <brad@divisionbyzero.net>
2024-05-16 16:31:26 -0700

    Update handling of the Perl Environment
    Use `local::lib` when it's installed and configured.

--- bash.d/95-perl.sh
+++ bash.d/95-perl.sh
@@ -1,4 +1,6 @@
 # Perl Brew
 if [ -f ~/perl5/perlbrew/etc/bashrc ]; then
     source ~/perl5/perlbrew/etc/bashrc
+elif [ -d ~/perl5 ] && perl -Mlocal::lib &> /dev/null; then
+    eval $(perl -Mlocal::lib)
 fi
