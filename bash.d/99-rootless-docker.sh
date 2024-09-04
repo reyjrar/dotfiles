ddb44cb014b25d2205478b06104ade703019fda9
Brad Lhotsky <brad@divisionbyzero.net>
2024-09-04 14:13:24 -0700

    Add a comment in the rootless-docker setup

--- bash.d/99-rootless-docker.sh
+++ bash.d/99-rootless-docker.sh
@@ -1,3 +1,4 @@
+## Detect a local rootless docker runtime
 rootless_docker_sock="$XDG_RUNTIME_DIR/docker.sock";
 if [ -e "$rootless_docker_sock" ]; then
     export DOCKER_HOST="unix://$rootless_docker_sock"
