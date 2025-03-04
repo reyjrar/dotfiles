0e9b28f29d5dd32db7a8a8b1a42789643cb8830d
Brad Lhotsky <brad@divisionbyzero.net>
2025-03-03 20:36:48 -0800

    Path fixes for zsh

--- shell.d/02-base-paths.sh
+++ shell.d/02-base-paths.sh
@@ -7,7 +7,9 @@ path_inject /usr/local/sbin
 path_inject /usr/local/bin
 path_inject /opt/local/sbin
 path_inject /opt/local/bin
+path_inject /opt/homebrew/bin
 path_inject /var/ossec/bin
+path_inject $HOME/go/bin
 path_inject $HOME/bin
 path_inject $HOME/.local/bin
 
